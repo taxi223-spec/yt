1ec827e7e8906a09ce7253363713e1e76d89fd99
taxi223-spec <taxi223@gmail.com>
2025-11-10 13:06:08 +0545

    Update travel.py

--- travel.py
+++ travel.py
@@ -3,7 +3,7 @@ import requests
 from datetime import datetime, timedelta
 
 # YouTube API Key
-API_KEY = "Enter your API Key here"
+API_KEY = "AIzaSyBJjgYVASelNgVKBy8j5oQDelkPVCHPK2Q"
 YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search"
 YOUTUBE_VIDEO_URL = "https://www.googleapis.com/youtube/v3/videos"
 YOUTUBE_CHANNEL_URL = "https://www.googleapis.com/youtube/v3/channels"

2615ff065a28c5dbb12769bfa5aab7f2bc7ffa95
taxi223-spec <taxi223@gmail.com>
2025-11-10 13:43:37 +0545

    Update travel.py

--- travel.py
+++ travel.py
@@ -122,7 +122,7 @@ if st.button("Fetch Data"):
                 views = int(stat["statistics"].get("viewCount", 0))
                 subs = int(channel["statistics"].get("subscriberCount", 0))
 
-                if subs > 1000:  # Only include channels with fewer than 3,000 subscribers
+                if subs > 10,000:  # Only include channels with fewer than 10,000 subscribers
                     all_results.append({
                         "Title": title,
                         "Description": description,
